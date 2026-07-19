76044913c9d9ee74ed9b8fd3b4d8d64614f23e1a
survi21 <sin.survi@gmail.com>
2026-07-19 14:16:06 +0530

    Rename logo file to lowercase extension

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -19,11 +19,7 @@ const userSchema = new mongoose.Schema(
     required: true,
   },
 
-  // role: {
-  //   type: String,
-  //   default: "user",
-  // },
-  
+ 
 role: {
   type: String,
   enum: ["user", "admin"],  // ✅ better control
